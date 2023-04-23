7c8d050d4b92223b3373d22691e6e3665925c2cb
shimaaAshraf <shimaaashraf402@gmail.com>
2023-04-23 00:42:23 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -14,14 +14,14 @@ n = rand() - RAND_MAX / 2;
 ld = n % 10;
 if (ld > 5)
 {
-printf("last digit of %d and is greater than 5", n);
+printf("Last digit of %d and is d% and is  greater than 5", n, ld);
 }
 else if (ld == 0)
-{printf("last digit of %d is %d and is 0", n, ld);
+{printf("Last digit of %d is %d and is 0", n, ld);
 }
 else
 {
-printf("last digit of %d is %d and less than 6 and not 0", n, ld);
+printf("Last digit of %d is %d and less than 6 and not 0", n, ld);
 }
 return (0);
 }
