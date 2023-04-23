04ba03063b02562a44446b28e46bbfa3b5048cf4
shimaaAshraf <shimaaashraf402@gmail.com>
2023-04-23 14:05:10 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+* main - Entry point
+* Description: 'print numbers of base 16'
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int n;
+int m;
+
+for (n = 48; n <= 57; n++)
+{
+putchr(n);
+}
+putchar(m);
+}
+putchar('\n');
+return (0);
+}
