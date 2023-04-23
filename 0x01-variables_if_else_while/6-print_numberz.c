8945fd2fef5f9da08d64b832c59bc57c1d301736
shimaaAshraf <shimaaashraf402@gmail.com>
2023-04-23 13:18:42 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+/**
+* main - Entry point
+* Description: 'print number of base 10 using putchar'
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int n;
+
+for (n = 0; n <= 9; n++)
+{
+putcha(n + 48);
+}
+putchar('\n');
+return (0);
+}
