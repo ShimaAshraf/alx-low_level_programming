5442af95c75ce6409338b0d65f62155038d12a8a
shimaaAshraf <shimaaashraf402@gmail.com>
2023-05-04 14:26:16 -0700

    7-leet.c

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+* leet - Entry point
+* Description: to check if letter is uppercase
+* @str: string
+ Return: int
+*/
+char *leet(char *str)
+{
+int i, j;
+char letters[] = "AEOTLaeotl";
+char numbers[] = "4307143071";
+
+for (i = 0; str[i]; i++)
+{
+for (j = 0; letters[j]; j++)
+if (str[i] == letters[j])
+{
+str[i] = numbers[j];
+break;
+}
+}
+return (str);
+}
