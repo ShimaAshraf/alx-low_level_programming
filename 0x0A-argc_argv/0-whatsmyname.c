578c5b9bfb8ccb7a91461179389be3200851eef3
shimaaAshraf <shimaaashraf402@gmail.com>
2023-05-13 17:09:31 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: to check if letter is uppercase
+ * @argc: num of inputs
+ * @argv: inputs
+ * Return: int
+ */
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
