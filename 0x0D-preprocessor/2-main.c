ff66c975f514efeeac15293aaabbbbcb9b7f002c
shimaaAshraf <shimaaashraf402@gmail.com>
2023-05-22 14:48:23 -0700

    0-object_like_macro.h

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main- Entry point
+ * Description: to check if letter is uppercase
+ * Return: int
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
