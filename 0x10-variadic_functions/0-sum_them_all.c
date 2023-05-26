6142745cd2bcb91ff899e87a1086ab3737205f7c
shimaaAshraf <shimaaashraf402@gmail.com>
2023-05-26 04:56:09 -0700

    0-sum_them_all.c

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,20 +1,26 @@
 #include "variadic_functions.h"
+#include <stdarg.h>
 /**
- * sum_them_all - Entry point
- * Description: to check if letter is uppercase
- * @n: number
- * Return: int
+ * sum_them_all - adds all the numbers
+ * @n: the number of parameters passed
+ * Return: Always 0
  */
+
 int sum_them_all(const unsigned int n, ...)
 {
+	int sum;
 	unsigned int i;
-	int sum = 0;
-	va_list args;
+	va_list ap;
 
-	va_start(args, n);
+	va_start(ap, n);
+
 	if (n == 0)
 		return (0);
+
 	for (i = 0; i < n; i++)
-		sum += va_arg(args, int);
+		sum += va_arg(ap, int);
+
+	va_end(ap);
+
 	return (sum);
 }
