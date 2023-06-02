3f2f277578d57958af0a01c1a91e33cabe3a161f
shimaaAshraf <shimaaashraf402@gmail.com>
2023-06-02 12:49:04 -0700

    100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+/**
+ * freem- Entry point
+ * Description: to check if letter is uppercase
+ * Return: int
+ */
+void freem(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
